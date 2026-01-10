2143e522cb6919228f0995e457b2c247be784a75
asonkes <audrey.sonkes@gmail.com>
2026-01-10 11:29:59 +0100

    création des routes générales + routes avec 'id' pour categories et tasks

--- routes/category.router.js
+++ routes/category.router.js
@@ -4,8 +4,15 @@
 
 const categoryRouter = require('express').Router();
 
+/** Ici c'est pour toutes les catégories */
 categoryRouter.get('/', (req, res) => {
     res.send('Bienvenue sur la page des catégories', 200);
+});
+
+/** Ici c'est pour une catégorie en particulier */
+categoryRouter.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Bienvenue sur la page de la catégorie ${id}`);
 });
 
 module.exports = categoryRouter;
--- routes/task.router.js
+++ routes/task.router.js
@@ -4,8 +4,15 @@
 
 const taskRouter = require('express').Router();
 
+/** Ici c'est pour récupérer toutes les tâches */
 taskRouter.get('/', (req, res) => {
     res.send("Bienvenue sur la page des tâches", 200);
-})
+});
+
+/** Ici c'est pour récupérer une tâche précise */
+taskRouter.get('/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`Bienvenue sur la tâche ${id}`);
+});
 
 module.exports = taskRouter;
